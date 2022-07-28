b8e9372ea9bc4ab3100f3bd6733d809449045a98
Yevhenii-Kovalenko <yevhenii.kovalenko.work@gmail.com>
2022-07-28 14:54:31 +0300

    feat: add navigation for menu

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ function hambHandler(e) {
 function renderPopup() {
    popup.appendChild(menu);
 }
+
+
+//Прокрутка до розділів
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if(menuLinks.length > 0){
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", OnMenuLinkClick);
+   })
+};
+
+function OnMenuLinkClick(e){
+   const menuLink = e.target;
+   if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+      window.scrollTo({
+         top: gotoBlockValue,
+         behavior: "smooth"
+      });
+      e.preventDefault();
+   }
+}
